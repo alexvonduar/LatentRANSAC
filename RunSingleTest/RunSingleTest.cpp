dffafd901fcbd11c42bae257f0a38964ee4e6f19
Roee <rlit@users.noreply.github.com>
2018-06-02 17:12:49 +0300

    added PnP and Rigid3d to RunSingleTest.cpp

--- RunSingleTest/RunSingleTest.cpp
+++ RunSingleTest/RunSingleTest.cpp
@@ -3,6 +3,9 @@
 #include <windows.h>
 #endif
 
+#define USE_OPENGV
+
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -12,10 +15,13 @@
 #include "config/ConfigParams.h"
 #include "estimators/FundmatrixEstimator.h"
 #include "estimators/HomogEstimator.h"
-
+#if defined(USE_OPENGV)
+#include "estimators/PnPEstimator.h"
+#include "estimators/Rigid3dEstimator.h"
+#endif
 
 // helper functions
-bool readCorrsFromFile(std::string& inputFilePath, std::vector<double>& pointData, unsigned int& numPts)
+bool readCorrsFromFile(std::string& inputFilePath, std::vector<double>& pointData, unsigned int& numPts, bool is3D)
 {
 	// read data from from file
 	std::ifstream infile(inputFilePath.c_str());
@@ -25,12 +31,17 @@ bool readCorrsFromFile(std::string& inputFilePath, std::vector<double>& pointDat
 		return false;
 	}
 	infile >> numPts;
-	pointData.resize(6*numPts);
+	pointData.resize(6 * numPts);
 	for (unsigned int i = 0; i < numPts; ++i)
 	{
-		infile >> pointData[6*i] >> pointData[6*i+1] >> pointData[6*i+3] >> pointData[6*i+4];
-		pointData[6*i+2] = 1.0;
-		pointData[6*i+5] = 1.0;
+		if (is3D)
+			infile >> pointData[6 * i] >> pointData[6 * i + 1] >> pointData[6 * i + 2] >> pointData[6 * i + 3] >> pointData[6 * i + 4] >> pointData[6 * i + 5];
+		else // 2D
+		{
+			infile >> pointData[6 * i] >> pointData[6 * i + 1] >> pointData[6 * i + 3] >> pointData[6 * i + 4];
+			pointData[6 * i + 2] = 1.0;
+			pointData[6 * i + 5] = 1.0;
+		}
 	}
 	infile.close();
 	return true;
@@ -60,7 +71,7 @@ int main(int argc, char **argv)
 	if (argc < 3)
 	{
 		std::cerr << "Usage: RunSingleTest <estimation problem> <config file>" << std::endl;
-		std::cerr << "\t<estimation problem>: 0 (fundamental matrix), 1 (homography)" << std::endl;
+		std::cerr << "\t<estimation problem>: 0 (fundamental matrix), 1 (homography), 2 (pnp), 3 (rigid3d)" << std::endl;
 		std::cerr << "\t<config file>: full path to configuration file" << std::endl;
 		return(EXIT_FAILURE);
 	}
@@ -70,8 +81,22 @@ int main(int argc, char **argv)
 	// seed random number generator
 	srand((unsigned int)time(NULL));
 
-	// initialize the appropriate robust estimation problem
 	if (estimation_problem == 0)
+		std::cout << "------- Solving FUNDAMENTAL --------" << std::endl;
+	else if (estimation_problem == 1)
+		std::cout << "------- Solving HOMOGRAPHY --------" << std::endl;
+	else if (estimation_problem == 2)
+		std::cout << "------- Solving PNP --------" << std::endl;
+	else if (estimation_problem == 3)
+		std::cout << "------- Solving rigid-3D --------" << std::endl;
+	else
+	{
+		std::cerr << "Error - not an implemented estimation problem" << std::endl;
+		return(EXIT_FAILURE);
+	}
+
+	// initialize the appropriate robust estimation problem
+	if (estimation_problem == 0)  // FUNDAMENTAL //
 	{
 		// ------------------------------------------------------------------------
 		// initialize the fundamental matrix estimation problem
@@ -85,8 +110,9 @@ int main(int argc, char **argv)
 		fund->initParamsUSAC(cfg);
 
 		// read in input data points
+		bool is3D = false; // 2D pixels
 		std::vector<double> point_data;
-		if ( !readCorrsFromFile(cfg.fund.inputFilePath, point_data, cfg.common.numDataPoints) )
+		if (!readCorrsFromFile(cfg.fund.inputFilePath, point_data, cfg.common.numDataPoints, is3D))
 		{
 			return(EXIT_FAILURE);
 		}
@@ -109,13 +135,11 @@ int main(int argc, char **argv)
 
 		fund->initDataUSAC(cfg);
 		fund->initProblem(cfg, &point_data[0]);
-		if (!fund->solve())
-		{
+		if (!fund->solve())		
 			return(EXIT_FAILURE);
-		}
 
 		// write out results
-		size_t pos = (cfg.fund.inputFilePath).find_last_of("/\\");
+		unsigned int pos = (cfg.fund.inputFilePath).find_last_of("/\\");
 		std::string working_dir = (cfg.fund.inputFilePath).substr(0, pos + 1);
 		std::ofstream outmodel((working_dir + "F.txt").c_str());
 		for (unsigned int i = 0; i < 3; ++i)
@@ -139,7 +163,7 @@ int main(int argc, char **argv)
 		fund->cleanupProblem();
 		delete fund;
 
-	} else if (estimation_problem == 1) {
+	} else if (estimation_problem == 1) { // HOMOGRAPHY //
 		// ------------------------------------------------------------------------
 		// initialize the homography estimation problem
 		ConfigParamsHomog cfg;
@@ -153,8 +177,9 @@ int main(int argc, char **argv)
 		homog->initParamsUSAC(cfg);
 
 		// read in input data points
+		bool is3D = false; // 2D pixels
 		std::vector<double> point_data;
-		if ( !readCorrsFromFile(cfg.homog.inputFilePath, point_data, cfg.common.numDataPoints) )
+		if (!readCorrsFromFile(cfg.homog.inputFilePath, point_data, cfg.common.numDataPoints, is3D))
 		{
 			return(EXIT_FAILURE);
 		}
@@ -177,12 +202,10 @@ int main(int argc, char **argv)
 		homog->initDataUSAC(cfg);
 		homog->initProblem(cfg, &point_data[0]);
 		if (!homog->solve())
-		{
 			return(EXIT_FAILURE);
-		}
 
 		// write out results
-		size_t pos = (cfg.homog.inputFilePath).find_last_of("/\\");
+		unsigned int pos = (cfg.homog.inputFilePath).find_last_of("/\\");
 		std::string working_dir = (cfg.homog.inputFilePath).substr(0, pos + 1);
 		std::ofstream outmodel((working_dir + "H.txt").c_str());
 		for (unsigned int i = 0; i < 3; ++i)
@@ -205,7 +228,145 @@ int main(int argc, char **argv)
 		prosac_data.clear();
 		homog->cleanupProblem();
 		delete homog;
-	} else {
+	}
+#if defined(USE_OPENGV)
+	else if (estimation_problem == 2) { // PnP //
+		// ------------------------------------------------------------------------
+		// initialize the PnP estimation problem
+		ConfigParamsPnP cfg;
+		if (!cfg.initParamsFromConfigFile(cfg_file_path))
+		{
+			std::cerr << "Error during initialization" << std::endl;
+			return(EXIT_FAILURE);
+		}
+
+		PnPEstimator* pnp = new PnPEstimator;
+		pnp->initParamsUSAC(cfg);
+
+		// read in input data points
+		bool is3D = true; // 3d points and 2d given in homogeneous coordinates
+		std::vector<double> point_data;
+		if (!readCorrsFromFile(cfg.pnp.inputFilePath, point_data, cfg.common.numDataPoints, is3D))
+		{
+			return(EXIT_FAILURE);
+		}
+
+		// read in prosac data if required
+		std::vector<unsigned int> prosac_data;
+		if (cfg.common.randomSamplingMethod == USACConfig::SAMP_PROSAC)
+		{
+			prosac_data.resize(cfg.common.numDataPoints);
+			if (!readPROSACDataFromFile(cfg.prosac.sortedPointsFile, cfg.common.numDataPoints, prosac_data))
+			{
+				return(EXIT_FAILURE);
+			}
+			cfg.prosac.sortedPointIndices = &prosac_data[0];
+		}
+		else {
+			cfg.prosac.sortedPointIndices = NULL;
+		}
+
+		// set up the pnp estimation problem
+		pnp->initDataUSAC(cfg);
+		pnp->initProblem(cfg, &point_data[0]);
+
+		if (!pnp->solve())
+			return(EXIT_FAILURE);
+
+		// write out results
+		unsigned int pos = (cfg.pnp.inputFilePath).find_last_of("/\\");
+		std::string working_dir = (cfg.pnp.inputFilePath).substr(0, pos + 1);
+		std::ofstream outmodel((working_dir + "RT.txt").c_str());
+		for (unsigned int i = 0; i < 3; ++i)
+		{
+			for (unsigned int j = 0; j < 4; ++j)
+			{
+				outmodel << pnp->final_model_params_[i + 3*j] << " "; // save in transposed form
+			}
+		}
+		outmodel.close();
+		std::ofstream outinliers((working_dir + "inliers.txt").c_str());
+		for (unsigned int i = 0; i < cfg.common.numDataPoints; ++i)
+		{
+			outinliers << pnp->usac_results_.inlier_flags_[i] << std::endl;
+		}
+		outinliers.close();
+
+		// clean up
+		point_data.clear();
+		prosac_data.clear();
+		pnp->cleanupProblem();
+		delete pnp;
+
+	}
+	else if (estimation_problem == 3) { // Rigid 3D //
+										// ------------------------------------------------------------------------
+										// initialize the Rigid 3D estimation problem
+		ConfigParamsRigid3d cfg;
+		if (!cfg.initParamsFromConfigFile(cfg_file_path))
+		{
+			std::cerr << "Error during initialization" << std::endl;
+			return(EXIT_FAILURE);
+		}
+
+		Rigid3dEstimator* rigid3d = new Rigid3dEstimator;
+		rigid3d->initParamsUSAC(cfg);
+
+		// read in input data points
+		bool is3D = true; // 3d points
+		std::vector<double> point_data;
+		if (!readCorrsFromFile(cfg.rigid3d.inputFilePath, point_data, cfg.common.numDataPoints, is3D))
+			return(EXIT_FAILURE);
+
+		// read in prosac data if required
+		std::vector<unsigned int> prosac_data;
+		if (cfg.common.randomSamplingMethod == USACConfig::SAMP_PROSAC)
+		{
+			prosac_data.resize(cfg.common.numDataPoints);
+			if (!readPROSACDataFromFile(cfg.prosac.sortedPointsFile, cfg.common.numDataPoints, prosac_data))
+			{
+				return(EXIT_FAILURE);
+			}
+			cfg.prosac.sortedPointIndices = &prosac_data[0];
+		}
+		else {
+			cfg.prosac.sortedPointIndices = NULL;
+		}
+
+		// set up the rigid3d estimation problem
+		rigid3d->initDataUSAC(cfg);
+		rigid3d->initProblem(cfg, &point_data[0]);
+
+		if (!rigid3d->solve())
+			return(EXIT_FAILURE);
+
+		// write out results
+		unsigned int pos = (cfg.rigid3d.inputFilePath).find_last_of("/\\");
+		std::string working_dir = (cfg.rigid3d.inputFilePath).substr(0, pos + 1);
+		std::ofstream outmodel((working_dir + "RT.txt").c_str());
+		for (unsigned int i = 0; i < 3; ++i)
+		{
+			for (unsigned int j = 0; j < 4; ++j)
+			{
+				outmodel << rigid3d->final_model_params_[i + 3 * j] << " "; // save in transposed form
+			}
+		}
+		outmodel.close();
+		std::ofstream outinliers((working_dir + "inliers.txt").c_str());
+		for (unsigned int i = 0; i < cfg.common.numDataPoints; ++i)
+		{
+			outinliers << rigid3d->usac_results_.inlier_flags_[i] << std::endl;
+		}
+		outinliers.close();
+
+		// clean up
+		point_data.clear();
+		prosac_data.clear();
+		rigid3d->cleanupProblem();
+		delete rigid3d;
+	}
+#endif
+	else {
 		std::cout << "Estimation problem currently not implemented" << std::endl;
 	}
 
